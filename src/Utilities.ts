a002b028093420b12cd3a8619fcbc0256109760d
genbs <gennaro.bs@gmail.com>
2021-03-14 13:02:39 +0100

    remove unused

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -4,23 +4,6 @@ import { ERepetitionType, IRepetition } from './types/scene-child'
 
 import Vec2 from './math/Vec2'
 
-/**
- * @ignore
- */
-export const parseFunction = {
-	suffix: '$fn:',
-
-	parse: (data: any): any => {
-		return typeof data === 'function' && data.name !== 'SimpleAnimation' ? parseFunction.suffix + data.toString() : data
-	},
-
-	unparse: (data: any): any => {
-		return typeof data === 'string' && data.indexOf(parseFunction.suffix) === 0
-			? eval(data.substr(parseFunction.suffix.length))
-			: data
-	},
-}
-
 // isDef: (object: any): boolean => typeof object !== 'undefined' && object !== null,
 const measurement = typeof performance !== 'undefined' ? performance : Date
 /**
