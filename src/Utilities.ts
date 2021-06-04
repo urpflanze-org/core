c6da2f686023e1b3af4ed7140535832de9fb0f27
genbs <gennaro.bs@gmail.com>
2021-06-04 11:14:24 +0200

    interpolate with recursion

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -144,6 +144,73 @@ export function noise(seed = 'random', x = 0, y = 0, z = 0): number {
 }
 
 /**
+ * Random number generator
+ */
+
+const randoms: {
+	[key: string]: () => number
+} = {}
+
+/**
+ * random number generator
+ * @param seed
+ * @returns
+ */
+export function random(seed?: string | number) {
+	const key: string = seed + ''
+
+	if (typeof randoms[key] === 'undefined') {
+		const seed = xmur3(key)
+		randoms[key] = sfc32(seed(), seed(), seed(), seed())
+	}
+
+	return randoms[key]()
+}
+
+/**
+ *
+ * @internal
+ * @param str
+ * @returns
+ */
+function xmur3(str: string) {
+	let i = 0,
+		h = 1779033703 ^ str.length
+	for (; i < str.length; i++) (h = Math.imul(h ^ str.charCodeAt(i), 3432918353)), (h = (h << 13) | (h >>> 19))
+
+	return function () {
+		h = Math.imul(h ^ (h >>> 16), 2246822507)
+		h = Math.imul(h ^ (h >>> 13), 3266489909)
+		return (h ^= h >>> 16) >>> 0
+	}
+}
+
+/**
+ * @internal
+ * @param a
+ * @param b
+ * @param c
+ * @param d
+ * @returns
+ */
+function sfc32(a: number, b: number, c: number, d: number) {
+	return function () {
+		a >>>= 0
+		b >>>= 0
+		c >>>= 0
+		d >>>= 0
+		let t = (a + b) | 0
+		a = b ^ (b >>> 9)
+		b = (c + (c << 3)) | 0
+		c = (c << 21) | (c >>> 11)
+		d = (d + 1) | 0
+		t = (t + d) | 0
+		c = (c + t) | 0
+		return (t >>> 0) / 4294967296
+	}
+}
+
+/**
  * Return angle (atan) from offset (or center) for matrix repetition.
  * Offset is array between [-1, -1] and [1, 1].
  * The return value is between -Math.PI / 2 and Math.PI / 2
@@ -222,6 +289,43 @@ export function distanceFromRepetition(repetition: IRepetition, offsetFromCenter
 	return 1
 }
 
+/// Interpolation
+
+/**
+ *
+ * @param from
+ * @param to
+ * @returns
+ */
+export function prepareBufferForInterpolation(
+	from: Float32Array,
+	to: Float32Array,
+	bClosed: boolean
+): [Float32Array, Float32Array] {
+	const fromBufferLength = from.length
+	const toBufferLength = to.length
+
+	if (fromBufferLength === toBufferLength) {
+		return [from, to]
+	}
+
+	const maxBufferLength = fromBufferLength > toBufferLength ? fromBufferLength : toBufferLength
+	const difference = Math.abs(fromBufferLength - toBufferLength)
+	const minBufferLength = maxBufferLength - difference
+
+	/////
+
+	const b = fromBufferLength < toBufferLength ? to : from
+	const t = fromBufferLength < toBufferLength ? from : to
+
+	const a = distributePointsInBuffer(t, Math.floor(difference / 2), bClosed)
+
+	// a[maxBufferLength - 2] = t[minBufferLength - 2]
+	// a[maxBufferLength - 1] = t[minBufferLength - 1]
+
+	return fromBufferLength > toBufferLength ? [b, a] : [a, b]
+}
+
 /**
  *
  * @param from
@@ -232,107 +336,125 @@ export function distanceFromRepetition(repetition: IRepetition, offsetFromCenter
 export function interpolate(
 	from: Float32Array,
 	to: Float32Array,
-	initialOffset: number | Array<number> = 0.5
+	initialOffset: number | Array<number> = 0.5,
+	bClosed = false
 ): Float32Array {
-	const fromBufferLength = from.length
-	const toBufferLength = to.length
-	const maxBufferLength = fromBufferLength > toBufferLength ? fromBufferLength : toBufferLength
-	const difference = Math.abs(fromBufferLength - toBufferLength)
-	const minBufferLength = maxBufferLength - difference
-
+	const [a, b] = prepareBufferForInterpolation(from, to, bClosed)
+	const maxBufferLength = Math.max(a.length, b.length)
 	const offset = typeof initialOffset === 'number' ? [initialOffset] : initialOffset
+	const maxPoints = maxBufferLength / 2
 
-	if (offset.length !== maxBufferLength) {
+	if (offset.length !== maxPoints) {
 		const tl = offset.length
-		for (let i = 0; i < maxBufferLength; i++) {
+		for (let i = 0; i < maxPoints; i++) {
 			offset[i] = offset[i % tl]
 		}
 	}
-
-	if (fromBufferLength === toBufferLength) {
-		const result = new Float32Array(fromBufferLength)
-
-		for (let i = 0; i < fromBufferLength; i += 2) {
-			result[i] = (1 - offset[i]) * from[i] + offset[i] * to[i]
-			result[i + 1] = (1 - offset[i]) * from[i + 1] + offset[i] * to[i + 1]
-		}
-
-		return result
-	}
-
-	/////
-
-	const b = fromBufferLength < toBufferLength ? to : from
-	const t = fromBufferLength < toBufferLength ? from : to
-
-	let settedLength = t.length
-	const a = new Float32Array(maxBufferLength)
-
-	/////
-
-	if (difference > minBufferLength) {
-		const lastPoint = minBufferLength - 2
-		const newPointsOnEdge = Math.floor(difference / lastPoint)
-		const newPointOffset = 1 / (newPointsOnEdge + 1)
-
-		for (let i = 0, j = 0; i < minBufferLength; i += 2, j += 2 + newPointsOnEdge * 2) {
-			const ax = t[i]
-			const ay = t[i + 1]
-			const bx = t[i + 2]
-			const by = t[i + 3]
-
-			a[j] = ax
-			a[j + 1] = ay
-
-			if (i < lastPoint) {
-				for (let h = 0; h < newPointsOnEdge; h++) {
-					const o = newPointOffset * (h + 1)
-					a[j + 2 + h * 2] = (1 - o) * ax + o * bx
-					a[j + 2 + h * 2 + 1] = (1 - o) * ay + o * by
-				}
-			} else {
-				a[j + 2] = ax
-				a[j + 3] = ay
-			}
-		}
-
-		settedLength = minBufferLength + newPointsOnEdge * lastPoint + 2
-	} else {
-		a.set(t)
-	}
-
-	////
-
-	const remainingPoints = (maxBufferLength - settedLength) / 2
-
-	if (remainingPoints > 0) {
-		const clone = new Float32Array(settedLength)
-		clone.set(a.subarray(0, settedLength))
-		let offset = Math.round(settedLength / (remainingPoints * 2)) * 2
-		if (offset % 2 === 1) offset++
-
-		for (let i = 0; i < remainingPoints; i++) {
-			const startIndex = i * offset
-			const endIndex = (i + 1) * offset
-			const d = endIndex - startIndex
-			a.set(clone.subarray(startIndex, endIndex), startIndex + i * 2)
-
-			a[startIndex + i * 2 + d] = 0.5 * clone[endIndex - 2] + 0.5 * clone[endIndex]
-			a[startIndex + i * 2 + d + 1] = 0.5 * clone[endIndex - 1] + 0.5 * clone[endIndex + 1]
-		}
-	}
-
-	a[maxBufferLength - 2] = t[minBufferLength - 2]
-	a[maxBufferLength - 1] = t[minBufferLength - 1]
 
 	////
 
 	const result = new Float32Array(maxBufferLength)
 
-	for (let i = 0; i < maxBufferLength; i += 2) {
-		result[i] = (1 - offset[i]) * a[i] + offset[i] * b[i]
-		result[i + 1] = (1 - offset[i]) * a[i + 1] + offset[i] * b[i + 1]
+	for (let i = 0, off = 0; i < maxBufferLength; i += 2, off++) {
+		result[i] = (1 - offset[off]) * a[i] + offset[off] * b[i]
+		result[i + 1] = (1 - offset[off]) * a[i + 1] + offset[off] * b[i + 1]
 	}
 
+	return result
+}
+
+export function distributePointsInBuffer(
+	buffer: Float32Array,
+	count: number,
+	bClosed: boolean,
+	additionalPoint?: Float32Array
+): Float32Array {
+	const bufferLen = buffer.length
+	const pointsLen = bufferLen / 2
+	const closedPointsLen = pointsLen - (bClosed ? 1 : 0)
+
+	if (closedPointsLen > 1 && count >= closedPointsLen) {
+		const finalBufferLength = (pointsLen + count) * 2
+		const lastPoint = bufferLen - 2
+		const newPointsOnEdge = Math.floor(count / pointsLen)
+		const newPointOffset = 1 / (newPointsOnEdge + 1)
+		const result = new Float32Array(bufferLen + newPointsOnEdge * lastPoint + 2)
+
+		for (let i = 0, j = 0; i < bufferLen; i += 2, j += 2 + newPointsOnEdge * 2) {
+			const ax = buffer[i]
+			const ay = buffer[i + 1]
+			const bx = buffer[i + 2]
+			const by = buffer[i + 3]
+
+			result[j] = ax
+			result[j + 1] = ay
+
+			if (i < lastPoint) {
+				for (let h = 0; h < newPointsOnEdge; h++) {
+					const o = newPointOffset * (h + 1)
+					result[j + 2 + h * 2] = (1 - o) * ax + o * bx
+					result[j + 2 + h * 2 + 1] = (1 - o) * ay + o * by
+				}
+			} else {
+				result[j + 2] = ax
+				result[j + 3] = ay
+			}
+		}
+
+		const remainingPoints = (finalBufferLength - result.length) / 2
+
+		return remainingPoints > 0
+			? distributePointsInBufferRecursive(result, remainingPoints, bClosed, additionalPoint)
+			: result
+	}
+
+	return distributePointsInBufferRecursive(buffer, count, bClosed, additionalPoint)
+}
+
+export function distributePointsInBufferRecursive(
+	buffer: Float32Array,
+	count: number,
+	bClosed: boolean,
+	additionalPoint?: Float32Array
+): Float32Array {
+	if (count < 1) {
+		return buffer
+	}
+
+	const bufferLength = buffer.length
+	let leftCount = Math.floor(bufferLength / 2)
+	// leftCount -= leftCount % 2 === 1 ? 1 : 0
+	leftCount += leftCount % 2 === 1 ? 1 : 0
+	const left = buffer.subarray(0, leftCount)
+	const right = buffer.subarray(leftCount, bufferLength)
+
+	if (count === 1) {
+		if (buffer.length === 2 && additionalPoint) {
+			return Float32Array.from([
+				buffer[0],
+				buffer[1],
+				0.5 * buffer[0] + 0.5 * additionalPoint[0],
+				0.5 * buffer[1] + 0.5 * additionalPoint[1],
+			])
+		}
+
+		const result = new Float32Array(bufferLength + 2)
+		result.set(left, 0)
+		result[leftCount] = 0.5 * buffer[leftCount - 2] + 0.5 * buffer[leftCount]
+		result[leftCount + 1] = 0.5 * buffer[leftCount - 1] + 0.5 * buffer[leftCount + 1]
+		// result[leftCount] = 0.5 * buffer[leftCount] + 0.5 * buffer[leftCount + 2]
+		// result[leftCount + 1] = 0.5 * buffer[leftCount + 1] + 0.5 * buffer[leftCount + 3]
+		result.set(right, leftCount + 2)
+		return result
+	}
+
+	const forPart = Math.floor(count / 2)
+
+	const leftResult = distributePointsInBuffer(left, count - forPart, bClosed, right.subarray(0, 2))
+	const rightResult = distributePointsInBuffer(right, forPart, bClosed, left.subarray(-2))
+
+	const result = new Float32Array(leftResult.length + rightResult.length)
+	result.set(leftResult, 0)
+	result.set(rightResult, leftResult.length)
 	return result
 }
