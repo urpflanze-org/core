71d6523224772ba0c6e197b9c112a4a78740cfb4
genbs <gennaro.bs@gmail.com>
2021-07-31 13:58:00 +0200

    feat: add noise min max value

--- src/Utilities.ts
+++ src/Utilities.ts
@@ -124,14 +124,17 @@ const noises: {
  * @param {number} [x=0]
  * @param {number} [y=0]
  * @param {number} [z=0]
+ * @param {number} [min=-1]
+ * @param {number} [max=-1]
  * @returns {number} between -1 and 1
  */
-export function noise(seed = 'random', x = 0, y = 0, z = 0): number {
+export function noise(seed = 'random', x = 0, y = 0, z = 0, min: -1, max: 1): number {
 	if (typeof noises[seed] === 'undefined') {
 		noises[seed] = new SimplexNoise(seed)
 	}
+	const value = noises[seed].noise3D(x, y, z)
 
-	return noises[seed].noise3D(x, y, z)
+	return min !== -1 || max !== 1 ? (0.5 + value * 0.5) * (max - min) + min : value
 }
 
 /**
